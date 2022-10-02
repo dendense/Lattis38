cbc32fdf01c096302e9bbe5ded85ef7bc9f79663
dendense <denyfarras@gmail.com>
2022-10-02 23:43:32 +0700

    Adding CSS

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div class="container" style="margin-top : 200px" align="center">
         <div class="mt-5 rounded" style="width: 700px">
             <div class="mb-3 h3">Selamat Datang di SKADIK 501</div>
-            <p>Masuk Untuk Mengakses Panel Skadik 501</p>
+            <p>Masuk Untuk Mengakses Panel Skadik 501.</p>
             <a href="login.php" class="btn btn-primary">Login</a>
         </div>
     </div>
