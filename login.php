14b23ffb9d75c9b0dde175740dca045f75759f7f
LuthfiDwi21 <luthfidwinur2121.com>
2022-10-05 00:20:17 +0700

    New

--- login.php
+++ login.php
@@ -1,8 +1,5 @@
 <?php
 include 'config.php';
-<<<<<<< HEAD
-if()
-=======
 session_start();
 if(isset($_SESSION['roles'])){
     if($_SESSION['roles'] == "admin"){
@@ -13,7 +10,6 @@ if(isset($_SESSION['roles'])){
         echo "Anda harus login untuk mengakses halaman admin";
     }
 }
->>>>>>> 5b98584575f5db47a01375cad9e308e4712c4b49
 ?>
 
 <html>
