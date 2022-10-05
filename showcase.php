079e470398b7990ca2c649d79b37225508351791
dendense <denyfarras@gmail.com>
2022-10-05 18:31:37 +0700

    Update

--- showcase.php
+++ showcase.php
@@ -6,10 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SHOWCASE SKADIK501</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
 
 <body>
-
+    <div class="container-fluid p-4">
+        <div class="row g-0">
+            <div class="col-4">
+                <text class="h4">LIST PENDIDIKAN</text>
+                <div class="row g-0">
+                    <div class=" col-8">
+                        <div class="p-3 border bg-light">SEJURBA PDE</div>
+                        <div class="p-3 border bg-light">SEJURBA PDE</div>
+                        <div class="p-3 border bg-light">SEJUSARTA PDE</div>
+                        <div class="p-3 border bg-light">SUSPA INTELUD</div>
+                        <div class="p-3 border bg-light">SUSBA SIBER</div>
+                        <div class="p-3 border bg-light">SESARCAB SUS</div>
+                        <div class="p-3 border bg-light">DIKLATSARJEMEN PNS</div>
+                        <div class="p-3 border bg-light"><b>TOTAL</b></div>
+                    </div>
+                    <div class="col-4 text-center">
+                        <div class="p-3 border bg-light">50 SISWA</div>
+                        <div class="p-3 border bg-light">50 SISWA</div>
+                        <div class="p-3 border bg-light">50 SISWA</div>
+                        <div class="p-3 border bg-light">50 SISWA</div>
+                        <div class="p-3 border bg-light">50 SISWA</div>
+                        <div class="p-3 border bg-light">50 SISWA</div>
+                        <div class="p-3 border bg-light">50 SISWA</div>
+                        <div class="p-3 border bg-light">201 SISWA</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row g-0">
+                    <div><text class="h4" style="padding-left: 1rem ">RENGIAT SKADIK 501 OKTOBER</text>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </body>
 
 </html>
